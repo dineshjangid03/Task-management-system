59f87645eb7cd50337385ba70fa80ff4ba156abb
dineshjangid03 <dinesh.jangid323@gmail.com>
2023-06-18 19:53:30 +0530

    added exception handler and custom exception classes

---
+++ TaskManagementSystem/src/main/java/com/musterdekho/exception/TaskNotFoundException.java
@@ -0,0 +1,17 @@
+package com.musterdekho.exception;
+
+public class TaskNotFoundException extends Exception{
+
+	public TaskNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TaskNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
